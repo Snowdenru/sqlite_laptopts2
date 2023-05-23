4a8757de183e7495f85277e4ea388881a71592d8
Zakabaka <artyomkosyakov86@gmail.com>
2023-05-23 22:28:04 +0400

    hot fix of data

--- SQL_insert.py
+++ SQL_insert.py
@@ -4,41 +4,41 @@ data = []
 with open ("company.txt",'r') as f:
     for line in f:
         item = line.strip().split(',')
-        data.append(tuple(int(item[0]), item[1]))
+        data.append(tuple([int(item[0]), item[1]]))
 print(data)
 
 conn = sqlite3.connect('laptops.db')
 cursor = conn.cursor()
 
-#cursor.executemany("INSERT INTO Company VALUES(?,?)", data)
-#conn.commit()
+cursor.executemany("INSERT INTO Company VALUES(?,?)", data)
+conn.commit()
 
 data = []
 with open ("DGMT.txt",'r') as f:
     for line in f:
         item = line.strip().split(',')
-        data.append(tuple(int(item[0]), item[1]))
+        data.append(tuple([int(item[0]), item[1]]))
 print(data)
 
 
-#cursor.executemany("INSERT INTO RAM VALUES(?,?)", data)
-#conn.commit()
+cursor.executemany("INSERT INTO RAM VALUES(?,?)", data)
+conn.commit()
 
 data = []
 with open ("operating_system.txt",'r') as f:
     for line in f:
         item = line.strip().split(',')
-        data.append(tuple(int(item[0]), item[1]))
+        data.append(tuple([int(item[0]), item[1]]))
 print(data)
 
-#cursor.executemany("INSERT INTO OS VALUES(?,?)", data)
-#conn.commit()
+cursor.executemany("INSERT INTO OS VALUES(?,?)", data)
+conn.commit()
 
 data = []
 with open ("series.txt",'r') as f:
     for line in f:
         item = line.strip().split(',')
-        #data.append(tuple(int(item[0]), item[1], int(item[2])))
+        data.append(tuple([int(item[0]), item[1], int(item[2])]))
         print(item)
 print(data)
 
@@ -49,7 +49,7 @@ data = []
 with open ("model.txt",'r') as f:
     for line in f:
         item = line.strip().split(',')
-        data.append(tuple(int(item[0]), item[1], int(item[2]), int(item[3]),int(item[4])))
+        data.append(tuple([int(item[0]), item[1], int(item[2]), int(item[3]),int(item[4])]))
 print(data)
 
 cursor.executemany("INSERT INTO Model VALUES(?,?,?,?,?)", data)
